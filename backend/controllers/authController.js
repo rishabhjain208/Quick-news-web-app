07a6cc151aa62fb3369691c60ae1eeb1ea8c2116
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-09-28 23:19:47 +0530

    add login and register controllers

---
+++ backend/controllers/authController.js
@@ -0,0 +1,61 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Register
+module.exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(403)
+        .json({ success: false, message: "User already registered !" });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username: username,
+      email: req.body.email,
+      password: hash,
+    });
+
+    const saved_user = await newUser.save();
+    const token = jwt.sign({ id: saved_user._id }, process.env.JWT_SECRET);
+    return res
+      .status(200)
+      .json({ success: true, message: "Successfull created", newUser, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+module.exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not Found" });
+    }
+
+    const checkpassword = await bcrypt.compare(password, user.password);
+    if (!checkpassword) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Password is Incorrect!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    return res
+      .status(200)
+      .json({ success: true, message: "Login Successfully!", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
